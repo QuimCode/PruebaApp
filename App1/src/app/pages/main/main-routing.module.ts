10f62910ab6d781ac2ca1b48ea99ca07fa8d848c
quimafoncode@gmail.com <quimafoncode@gmail.com>
2024-04-19 21:38:43 -0300

    V.0.0.2

---
+++ App1/src/app/pages/main/main-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MainPage } from './main.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPage
+  },  {
+    path: 'sign-up',
+    loadChildren: () => import('./sign-up/sign-up.module').then( m => m.SignUpPageModule)
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () => import('./forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class MainPageRoutingModule {}
---
+++ App1/src/app/pages/main/main.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.page.html',
+  styleUrls: ['./main.page.scss'],
+})
+export class MainPage implements OnInit {
+
+  form = new FormGroup({
+    email: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('',[Validators.required])
+  })
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  submit()
+
+}
